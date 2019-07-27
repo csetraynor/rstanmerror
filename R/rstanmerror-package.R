f7b5d851fab4b864aefa312cdea3872013961ce9
Serra Traynor, Carlos <carlos.serratraynor@astrazeneca.com>
2019-07-27 18:36:55 +0000

    first commit

---
+++ R/rstanmerror-package.R
@@ -0,0 +1,16 @@
+#' The 'rstanmerror' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name rstanmerror-package
+#' @aliases rstanmerror
+#' @useDynLib rstanmerror, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.1. https://mc-stan.org
+#'
+NULL
